0a90a9836dfe48e3b02b3bd78641d4afdae9c873
Edd Hannay <edd@vega.xyz>
2022-07-04 19:21:34 +0100

    Better errors

--- src/lib/check.mjs
+++ src/lib/check.mjs
@@ -1,9 +1,9 @@
 // Replaces the GraphQL port defined in networks.toml with the REST endpoint
-export const statsWeCareAbout = ['blockHeight', 'totalPeers']
 import * as sha256 from 'fast-sha256'
 import stringify from 'fast-json-stable-stringify'
 import sortBy from 'lodash.sortby'
 import { TextEncoder } from 'util'
+export const statsWeCareAbout = ['blockHeight', 'totalPeers']
 
 const query = `{
   statistics {
@@ -38,51 +38,80 @@ export async function fetchStats (urlFromConfig) {
 
   try {
     res = await fetch(url, {
-      method: "POST",
+      method: 'POST',
       body: JSON.stringify({
         variables: null,
-        query: query
+        query
       }),
       headers: {
         'Content-Type': 'application/json',
-        'Origin': urlFromConfig
-      } 
+        Origin: urlFromConfig
+      }
     })
-
 
     stats = await res.json()
   } catch (e) {
-    console.debug(`Failed to fetch ${urlFromConfig}`)
+    console.debug(`Failed to fetch ${urlFromConfig} (${e.message} [${e.cause.code}])`)
+    return fakeCheck(urlFromConfig)
   }
 
+  if (!stats) {
+    console.debug(`Failed to fetch ${urlFromConfig} (Empty result)`)
+    return fakeCheck(urlFromConfig)
+  }
+
+  if (stats && stats.errors) {
+    console.debug(`Failed to fetch ${urlFromConfig} (${JSON.stringify(stats.errors)})`)
+    return fakeCheck(urlFromConfig)
+  }
   return check(urlFromConfig, stats)
 }
 
 // Chef produces a shortened hash of some data. It is a bad function name.
-export function chef(object) {
-    return new Buffer.from(sha256.hash(stringify(object))).toString('hex').substr(-6)
+export function chef (object) {
+  return new Buffer.from(sha256.hash(stringify(object))).toString('hex').substr(-6)
+}
+
+function fakeCheck (url) {
+  const res = {
+    host: cleanHostname(url)
+  }
+  statsWeCareAbout.forEach(key => res[key] = '-')
+  res.steakHash = '-'
+  res.startupHash = '-'
+  res.epochHash = '-'
+
+  return res
+}
+
+function cleanHostname (url) {
+  return url.replace('http://', '').replace('https://', '').replace(':3008', '').replace('.vega.community', '')
 }
 
 export function check (urlFromConfig, stats) {
-  const host = urlFromConfig.replace('http://', '').replace('https://', '').replace(':3008', '').replace('.vega.community', '')
+  const host = cleanHostname(urlFromConfig)
   const res = { host }
   try {
     statsWeCareAbout.forEach(key => res[key] = stats.data.statistics[key])
 
     const startupData = {
-      appVersion: stats.data.statistics['appVersion'],
-      genesisTime: stats.data.statistics['genesisTime'],
-      vegaTime: stats.data.statistics['vegaTime'],
-      chainId: stats.data.statistics['chainId']
+      appVersion: stats.data.statistics.appVersion,
+      genesisTime: stats.data.statistics.genesisTime,
+      vegaTime: stats.data.statistics.vegaTime,
+      chainId: stats.data.statistics.chainId
     }
 
+    const stakeValues = sortBy(stats.data.nodes, 'name')
     // Let's hash some data
-    res['steakHash'] = chef(sortBy(stats.data.nodes, 'name'))
-    res['startupHash'] = chef(startupData)
-    res['epochHash'] = chef(stats.data.epoch)
-
+    if (stats.data.nodes?.length > 0) {
+      res.steakHash = chef(stakeValues)
+    } else {
+      res.steakHash = '-'
+    }
+    res.startupHash = chef(startupData)
+    res.epochHash = chef(stats.data.epoch)
   } catch (e) {
-    console.debug(`Failed to parse ${urlFromConfig}`)
+    console.debug(`Failed to parse ${urlFromConfig} (${e.message}`)
     statsWeCareAbout.forEach(key => res[key] = '-')
   }
   return res
