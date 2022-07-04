91b0cd7865e43bd1b07a21f0337d8673d2037aa2
Edd Hannay <edd@vega.xyz>
2022-07-04 15:39:32 +0100

    Add current time

--- src/lib/check.mjs
+++ src/lib/check.mjs
@@ -1,28 +1,28 @@
 // Replaces the GraphQL port defined in networks.toml with the REST endpoint
-export const statsWeCareAbout = ['blockHeight', 'vegaTime', 'chainId', 'appVersion', 'genesisTime']
+export const statsWeCareAbout = ['blockHeight', 'chainId', 'appVersion', 'genesisTime', 'vegaTime', 'currentTime']
 
-export async function fetchStats(urlFromConfig) {
-	let url = `${urlFromConfig}/statistics`
-	let res, stats, data
+export async function fetchStats (urlFromConfig) {
+  const url = `${urlFromConfig}/statistics`
+  let res, stats
 
-	try {
-		res = await fetch(url)
-		stats = await res.json()
-	} catch (e) {
-		console.debug(`Failed to fetch ${urlFromConfig}`)
-	}
+  try {
+    res = await fetch(url)
+    stats = await res.json()
+  } catch (e) {
+    console.debug(`Failed to fetch ${urlFromConfig}`)
+  }
 
-	return data = check(urlFromConfig, stats)
+  return check(urlFromConfig, stats)
 }
 
-export function check(urlFromConfig, stats) {
-	const host = urlFromConfig.replace('http://', '').replace('https://', '')
-	const res = { host: host }
-	try {
-		statsWeCareAbout.map(key => res[key] = stats.statistics[key])
-	} catch (e) {
-		console.debug(`Failed to parse ${urlFromConfig}`)
-		statsWeCareAbout.map(key => res[key] = '-')
-	}
-	return res
+export function check (urlFromConfig, stats) {
+  const host = urlFromConfig.replace('http://', '').replace('https://', '')
+  const res = { host }
+  try {
+    statsWeCareAbout.forEach(key => res[key] = stats.statistics[key])
+  } catch (e) {
+    console.debug(`Failed to parse ${urlFromConfig}`)
+    statsWeCareAbout.forEach(key => res[key] = '-')
+  }
+  return res
 }
--- src/lib/output.mjs
+++ src/lib/output.mjs
@@ -1,6 +1,5 @@
-import { statsWeCareAbout } from './check.mjs'
 import { printTable } from 'console-table-printer'
 
-export async function output(stats) {
-	printTable(stats)	
+export async function output (stats) {
+  printTable(stats)
 }
